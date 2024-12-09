f98788691d14d0f758b6fa7c3d1dd6451a34a2a8
riyazattri <mdriyazuddine17@gmail.com>
2024-12-09 21:56:26 +0530

    UI navigation done

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+    // WIP  Server action Onboard the user
+    // WIP 200 || 201
+    return (
+        <div>Dashboard</div>
+    )
+}
+
+export default Page
